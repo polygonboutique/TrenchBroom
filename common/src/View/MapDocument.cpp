e70e849dd2250d24e8f021d4e54f714f46a13b07
Kristian Duske <kristian.duske@gmail.com>
2020-04-10 21:52:14 +0200

    Split Brush off of BrushNode
    This commit splits Brush and BrushNode into two classes. BrushNode
    retains its public interface, but delegates calls to Brush where
    necessary. For now, Brush still has a back reference to BrushNode, and
    that will be removed in a later commit.

--- common/src/View/MapDocument.cpp
+++ common/src/View/MapDocument.cpp
@@ -35,6 +35,7 @@
 #include "IO/SystemPaths.h"
 #include "Model/AttributeNameWithDoubleQuotationMarksIssueGenerator.h"
 #include "Model/AttributeValueWithDoubleQuotationMarksIssueGenerator.h"
+#include "Model/Brush.h"
 #include "Model/BrushNode.h"
 #include "Model/BrushBuilder.h"
 #include "Model/BrushGeometry.h"
@@ -668,7 +669,7 @@ namespace TrenchBroom {
         void MapDocument::selectFacesWithTexture(const Assets::Texture* texture) {
             Model::CollectSelectableBrushFacesVisitor visitor(*m_editorContext, [=](const Model::BrushFace* face) {
                 // FIXME: we shouldn't need this extra check here to prevent hidden brushes from being included; fix it in EditorContext
-                if (face->brush()->hidden()) {
+                if (face->brush()->node()->hidden()) {
                     return false;
                 }
                 return face->texture() == texture;
@@ -1187,7 +1188,7 @@ namespace TrenchBroom {
             if (!selectedNodes().brushes().empty()) {
                 parent = selectedNodes().brushes().front()->parent();
             } else if (!selectedBrushFaces().empty()) {
-                parent = selectedBrushFaces().front()->brush()->parent();
+                parent = selectedBrushFaces().front()->brush()->node()->parent();
             } else {
                 parent = currentParent();
             }
